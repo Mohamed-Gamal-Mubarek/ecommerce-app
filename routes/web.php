550743e8b137f4e3599116f59f3eb2411150451f
wolf <ahmedragabyasin2020@gmail.com>
2021-08-21 16:20:21 +0200

    livewire install and home page

--- routes/web.php
+++ routes/web.php
@@ -2,22 +2,14 @@
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers;
+use App\Http\Livewire\Home;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Auth::routes();
 
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('/home', [Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', Home::class);
