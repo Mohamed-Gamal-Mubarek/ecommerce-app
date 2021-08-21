f0c3e2dc696913a8628bbaaf308af889a00dd46f
wolf <ahmedragabyasin2020@gmail.com>
2021-08-21 16:59:46 +0200

    cart , shop , cheakout page

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,10 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers;
+use App\Http\Livewire\Cart;
+use App\Http\Livewire\Checkout;
 use App\Http\Livewire\Home;
+use App\Http\Livewire\Shop;
 
 // Route::get('/', function () {
 //     return view('welcome');
@@ -13,3 +16,6 @@ use App\Http\Livewire\Home;
 
 // Route::get('/home', [Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/', Home::class);
+Route::get('/shop',Shop::class);
+Route::get('/cart',Cart::class)->name('product.cart');
+Route::get('/checkout',Checkout::class);
